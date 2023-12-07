1528f84f06a88c9f4388c8b11f9fe6689052ddf0
betarunner <143918699+betarunner@users.noreply.github.com>
2023-12-06 21:04:05 -0800

    edited semester dates

--- generate_calendar_beta1.py
+++ generate_calendar_beta1.py
@@ -98,8 +98,8 @@ course_data = args.course_data.strip()
 semester = args.semester
 
 semester_dates = {
-    'ss1': {'start': '07/03/23', 'end': '08/05/23'},
-    'ss2': {'start': '08/07/23', 'end': '09/09/23'},
+    'ss1': {'start': '07/01/24', 'end': '08/03/24'},
+    'ss2': {'start': '08/05/24', 'end': '09/07/24'},
     'fall': {'start': '09/28/23', 'end': '12/16/23'},
     'winter': {'start': '01/08/24', 'end': '03/23/24'},
     'spring': {'start': '03/27/24', 'end': '06/16/24'}
